b236e465e71ad0f74ad13b93ab157ca9ee378d49
bhavindev <bhavinsen1992@gmail.com>
2025-06-05 15:17:26 +0530

    updated requirements file

--- core/settings.py
+++ core/settings.py
@@ -159,18 +159,17 @@ elif os.getenv('POSTGRES_DB') == 'True' and os.getenv('MYSQL_DB') == 'False':
         }
     }
 else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-            }
-    }
-    
-import dj_database_url
+    # DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #         }
+    # }
+    import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-}
+    DATABASES = {
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
